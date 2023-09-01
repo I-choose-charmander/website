37aec8629446ad3d099dcae00864772ea727dd30
Sheridan <116517473+I-choose-charmander@users.noreply.github.com>
2023-08-31 19:59:57 -0700

    new application
    v.1

---
+++ macro.js
@@ -0,0 +1,20 @@
+function BMR(){
+    var weight = document.getElementById("weight").value
+    var feet = document.getElementById("feet").value
+    var inches = document.getElementById("inches").value
+    var A = document.getElementById("age").value
+
+    var W = weight /  2.2
+    var H = (((feet * 12) + +inches) * 2.54)
+    
+    if(document.getElementById("msex").checked == true) {
+        var bmr = 66.47 + ( 13.75 * W) + ( 5.003 * H ) - ( 6.755 * A )
+        document.getElementById("bmr").innerHTML = bmr
+    }else if(document.getElementById("fsex").checked == true){
+        var bmr =  (665.1 + (9.563 + W) + (1.850  * H) - (4.676 * A))
+        document.getElementById("bmr").innerHTML = bmr
+    }else{
+        alert("please make a selection for sex")
+    }
+}
+
